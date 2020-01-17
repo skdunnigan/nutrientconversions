2e4e3ec8cfd8f4b29de62b94fe70cb3a5f77b519
Shannon Dunnigan <Shannon.Dunnigan@dep.state.fl.us>
2020-01-17 15:44:00 -0500

    added custom visual labels and colors script. Color codes need to still be updated...borrowed from another script

---
+++ R/00_vis-custom.R
@@ -0,0 +1,31 @@
+# visual customizations for plots
+
+# ----01 create custom labels for plots----
+
+# create label for chlorophyll plots
+chla_y_title <- expression(paste("Chlorophyll ", italic("a "), mu*"g/L"))
+# create label for phosphorus plots
+phos_y_title <- expression(paste("Total Phosphorus mg/L"))
+# create label for nitrogen plots
+nitro_y_title <- expression(paste("Total Nitrogen mg/L"))
+# create label for fecal plots
+fecal_y_title <- expression(paste("Fecal coliform CFU/100mL"))
+# create label for enterococcus plots
+entero_y_title <- expression(paste("Enterococcus MPN/100mL"))
+# create label for temperature title
+temp_y_title <- expression(paste("Temperature ("~degree*C*")"))
+
+
+# ----02 create color palette for sites----
+sitecolours <- c(
+  Micklers = "#F8766D",
+  `GL1` = "#D89000",
+  `GL2` = "#A3A500",
+  `Lake Middle` = "#39B600",
+  `GL4` = "#00BF7D",
+  `Lake South` = "#00BFC4",
+  `River North` = "#00B0F6",
+  `GR1` = "#9590FF",
+  `Guana River` = "#E76BF3",
+  `GR3` = "#FF62BC"
+)
